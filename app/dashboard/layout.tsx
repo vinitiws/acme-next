097bacc20f75ff7aeb0866742857be47f102a5ad
vinitiws <viniciusnicacio80@gmail.com>
2026-02-10 23:10:01 -0300

    Obter dados do card

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,11 +6,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
-        <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
-
-
-
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ import Image from 'next/image'
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape}>
+    <main className="flex min-h-screen flex-col p-6 ">
+      {/* <div className={styles.shape} /> */}
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -17,7 +18,7 @@ export default function Page() {
             className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
           />
           <p className={` ${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is the exam for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -40,7 +41,7 @@ export default function Page() {
             alt='Screenshots of the dashboard project showing desktop version'
           />
           <Image
-            src="/hero-desktop.png"
+            src="/hero-mobile.png"
             width={560}
             height={620}
             className='block md:hidden'
